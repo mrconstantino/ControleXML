4318776c6b00402eda3e5f2cda006cef5753ab37
Marcio <mrconstantino@gmail.com>
2022-03-21 21:22:56 -0300

    Add project files.

---
+++ ControleXML/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ControleXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbXML = new System.Windows.Forms.RichTextBox();
+            this.btnXML = new System.Windows.Forms.Button();
+            this.dtgXML = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgXML)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtbXML
+            // 
+            this.rtbXML.Location = new System.Drawing.Point(12, 12);
+            this.rtbXML.Name = "rtbXML";
+            this.rtbXML.Size = new System.Drawing.Size(776, 139);
+            this.rtbXML.TabIndex = 0;
+            this.rtbXML.Text = "";
+            // 
+            // btnXML
+            // 
+            this.btnXML.Location = new System.Drawing.Point(713, 313);
+            this.btnXML.Name = "btnXML";
+            this.btnXML.Size = new System.Drawing.Size(75, 23);
+            this.btnXML.TabIndex = 1;
+            this.btnXML.Text = "XML";
+            this.btnXML.UseVisualStyleBackColor = true;
+            this.btnXML.Click += new System.EventHandler(this.btnXML_Click);
+            // 
+            // dtgXML
+            // 
+            this.dtgXML.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgXML.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dtgXML.Location = new System.Drawing.Point(12, 157);
+            this.dtgXML.Name = "dtgXML";
+            this.dtgXML.Size = new System.Drawing.Size(776, 150);
+            this.dtgXML.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 300;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 347);
+            this.Controls.Add(this.dtgXML);
+            this.Controls.Add(this.btnXML);
+            this.Controls.Add(this.rtbXML);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgXML)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbXML;
+        private System.Windows.Forms.Button btnXML;
+        private System.Windows.Forms.DataGridView dtgXML;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ ControleXML/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ControleXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnXML_Click(object sender, EventArgs e)
+        {
+            XmlDocument DadosXml = new XmlDocument();
+
+            DadosXml.LoadXml(rtbXML.Text);
+
+            //if (DadosXml.DocumentElement.OuterXml.Contains("Signature"))
+            //{
+            //    MessageBox.Show("Signature encontrada.");
+            //}
+
+            //string IdEventoEnvio = DadosXml.DocumentElement.Attributes.Item(0).Value;
+            //string NomeEventoPrincipal = DadosXml.DocumentElement.Name;
+            //string NomeEvento = DadosXml.DocumentElement.FirstChild.Name;
+
+            //string XMLTexto = DadosXml.OuterXml;
+
+            //XmlElement Elementos = DadosXml.DocumentElement;
+
+            string NomeElemento = string.Empty;
+
+            using (var srXml = new StringReader(DadosXml.LastChild.OuterXml))
+            using (var readerXml = XmlReader.Create(srXml))
+            {
+                while (readerXml.Read())
+                {
+                    if (readerXml.NodeType == XmlNodeType.Element)
+                    {
+                        NomeElemento = readerXml.Name;
+                    }
+
+                    if (readerXml.NodeType == XmlNodeType.Text)
+                    {
+                        dtgXML.Rows.Add(NomeElemento, readerXml.Value);
+                    }
+                }
+            }
+        }
+
+        //private XmlDocument RemovaAtributosDesnecessarios(XmlDocument DadosXML)
+        //{
+        //    XmlElement XMLElement;
+
+        //    XMLElement = DadosXML.DocumentElement;
+
+        //    XMLElement.RemoveAttribute("xmlns:xsd");
+        //    XMLElement.RemoveAttribute("xmlns:xsi");
+        //    XMLElement.RemoveAttribute("xmlns:xsi");
+
+        //    DadosXML.LoadXml(XMLElement.OuterXml);
+
+        //    return DadosXML;
+        //}
+    }
+}
